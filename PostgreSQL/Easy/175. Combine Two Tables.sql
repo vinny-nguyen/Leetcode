6285fc2fc608aa4847702d4bfbca3186d2a5401b
Vincent Nguyen <81478088+vinny-nguyen@users.noreply.github.com>
2025-05-19 14:20:18 -0400

    Create 175. Combine Two Tables.sql

---
+++ PostgreSQL/Easy/175. Combine Two Tables.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/combine-two-tables --
+
+-- Write your PostgreSQL query statement below
+SELECT p.lastName, p.firstName, a.city, a.state 
+FROM Person p
+LEFT JOIN Address a ON p.personId = a.personId
