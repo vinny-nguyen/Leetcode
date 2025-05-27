c6a2a825cc154aaf49113db90cfbc0435b86955b
Vincent Nguyen <81478088+vinny-nguyen@users.noreply.github.com>
2025-05-27 13:49:08 -0400

    Create 2894. Divisible and Non-divisible Sums Difference.cs

---
+++ C#/Easy/2894. Divisible and Non-divisible Sums Difference.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int DifferenceOfSums(int n, int m) {
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int i = 1; i < n + 1; ++i) {
+            if (i % m != 0) {
+                num1 += i;
+            } else {
+                num2 += i;
+            }
+        }
+        return num1 - num2;
+    }
+}
