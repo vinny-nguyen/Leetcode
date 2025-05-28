cb394d0298ca78ba8bbe737321a878929e1c2611
Vincent Nguyen <81478088+vinny-nguyen@users.noreply.github.com>
2025-05-27 23:24:06 -0400

    Create 2703. Return Length of Arguments Passed.tsx

---
+++ TypeScript/Easy/2703. Return Length of Arguments Passed.tsx
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/return-length-of-arguments-passed
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
