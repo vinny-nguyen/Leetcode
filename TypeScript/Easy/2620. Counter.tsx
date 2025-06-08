33b391efb3e166607c8e3f8c4e6668edab99cddb
Vincent Nguyen <81478088+vinny-nguyen@users.noreply.github.com>
2025-06-08 12:45:38 -0400

    Update 2620. Counter.tsx

--- TypeScript/Easy/2620. Counter.tsx
+++ TypeScript/Easy/2620. Counter.tsx
@@ -1,11 +1,18 @@
 // https://leetcode.com/problems/counter/
 
 function createCounter(n: number): () => number {
+    let count = n; // makes count become mutable since ts function parameters are const by default
     return function() {
-        return n++;
+        return count++;
     }
 }
 
+/* Test:
+const counter = createCounter(5);
+console.log(counter()); // 5
+console.log(counter()); // 6
+console.log(counter()); // 7
+*/
 
 /** 
  * const counter = createCounter(10)
