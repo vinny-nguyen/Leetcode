f8d334631b6ee579aa4f1fb859c42dfe9c3fa378
Vincent Nguyen <81478088+vinny-nguyen@users.noreply.github.com>
2025-12-05 23:02:21 -0500

    Update 217. Contains Duplicate.py

--- Python3/Easy/217. Contains Duplicate.py
+++ Python3/Easy/217. Contains Duplicate.py
@@ -1,18 +1,9 @@
-# https://leetcode.com/problems/contains-duplicate/description/
-
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        duplicates = []
-        
-        for num in nums:
-            if num in seen:
-                duplicates.append(num)
-            else:
-                seen.add(num)
-
-        if len(duplicates) == 0:
-            return False
-        else:
-            return True
+# https://leetcode.com/problems/contains-duplicate/
+class Solution(object):
+    def containsDuplicate(self, nums):
+        return len(nums) != len(set(nums))
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
         
